a035f0bedfe1813048a17350280103842e6d6fea
Gabriel Tellaroli Ramos <gabriel.tellaroli@dp6inc.local>
2020-08-06 20:01:12 -0300

    Finalizei a pagina give-class, e já criei o script para add mais campos no form.

---
+++ scripts/addFields.js
@@ -0,0 +1,22 @@
+// Search for button
+const addTime = document.querySelector("#add-time");
+
+// When click the button "addTime" execute the arrowFunction
+addTime.addEventListener('click', () =>{
+
+    //Clone fields.
+    const newFieldContainer = document.querySelector(".schedule-item").cloneNode(true);
+
+    //Get all inputs into the "newFieldContainer"
+    const fields = newFieldContainer.querySelectorAll('input');
+
+    //Clean each field
+    fields.forEach( (field) =>{
+        field.value = "";
+    });
+
+    //Put "newFieldContainer" below the previous
+    document.querySelector("#schedule-items").appendChild(newFieldContainer);
+    
+})
+
